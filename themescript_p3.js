7e97e8bbb74a2f93f5d0e7ef44883f9d048e32d2
Wizzikz <rihikz@gmail.com>
2015-04-29 22:53:36 +0300

    P3 & RCS + themescript

---
+++ themescript_p3.js
@@ -0,0 +1,13 @@
+$.getScript("https://d1rfegul30378.cloudfront.net/files/plugCubed.js",       
+            
+            
+            function(){
+   alert("Script loaded and executed.");
+
+});
+            
+                        
+ $.getScript("https://wizzikz.github.io/themescript.js",           
+            function(){
+
+});
