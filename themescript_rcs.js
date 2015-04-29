bfa29fea8bb245aa11440d9a9eaea9455acdacc2
Wizzikz <rihikz@gmail.com>
2015-04-29 22:47:22 +0300

    More test (sorry)

--- themescript_rcs.js
+++ themescript_rcs.js
@@ -1,5 +1,8 @@
 
-$.getScript("https://wizzikz.github.io/themescript.js", function(){
-$.getScript("https://code.radiant.dj/rs.min.js", function()
+
+
+$.getScript("https://wizzikz.github.io/themescript.js","https://code.radiant.dj/rs.min.js" function(){
+    
+   alert("Script loaded and executed.");
 
 });
