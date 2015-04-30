85adce2b6b08baa17d0678817385b4190bad5313
Wizzikz <rihikz@gmail.com>
2015-04-30 10:06:25 +0300

    Added pluggedN launcher

---
+++ themescript_pn.js
@@ -0,0 +1,12 @@
+$.getScript("https://raw.githubusercontent.com/nthitz/pluggedN/master/bookmarklet.js",       
+            
+            
+            function(){
+
+});
+            
+                        
+ $.getScript("https://wizzikz.github.io/themescript.js",           
+            function(){
+
+});
