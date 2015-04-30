13ef5a1c2e9a9341d3810d3dbf3cb5187e76edc9
Wizzikz <rihikz@gmail.com>
2015-04-30 13:22:06 +0300

    Added userscripts

---
+++ usrscripts/ts_rcs.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name            Themescript UserScript
+// @description     Loads themescript and desired plugin
+// @include         https://plug.dj/*
+// @exclude         https://plug.dj/dashboard
+// @exclude         https://plug.dj/privacy
+// @exclude         https://plug.dj/terms
+// @exclude         https://plug.dj/_/*
+// @exclude         https://plug.dj/@/*
+// @exclude         https://plug.dj/ba
+// @exclude         https://plug.dj/plot
+// @exclude         https://plug.dj/about
+// @exclude         https://plug.dj/team
+// @exclude         https://plug.dj/jobs
+// @exclude         https://plug.dj/partners
+// @exclude         https://plug.dj/press
+// ==/UserScript==
+
+(function(){
+    var a = function(){
+        if (!document.getElementsByClassName('loading-box').length) setTimeout(a,200);
+        else b();
+    },
+    b = function(){
+        if (document.getElementsByClassName('loading-box').length) setTimeout(b,200);
+        else {
+            var c = document.createElement('script');
+            c.src = "https://wizzikz.github.io/themescript_rcs.js";
+            document.head.appendChild(c);
+        }
+    };
+    a();
+})();
