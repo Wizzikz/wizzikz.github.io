efbc357f58d5485b423bf11ab066758d3ec6104a
Wizzikz <rihikz@gmail.com>
2015-04-30 12:40:37 +0300

    Added tastyplug themescript

---
+++ themescript_tp.js
@@ -0,0 +1,12 @@
+$.getScript("https://fungustime.pw/tastyplug/tastyplug.js",       
+            
+            
+            function(){
+
+});
+            
+                        
+ $.getScript("https://wizzikz.github.io/themescript.js",           
+            function(){
+
+});
